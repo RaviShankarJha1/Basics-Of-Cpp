96c31d49c665721c318670825067886635bdf293
RaviShankarJha1 <rjha81467@gmail.com>
2025-10-01 01:54:56 +0530

    Create Calculator.cpp

---
+++ Calculator.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+      cout << "  Simple Calculator  " << endl;
+      float a,b;
+      cout << "Enter the first number: ";
+      cin >> a;
+      cout << "Enter the second number: ";
+      cin >> b;
+
+
+      float sum = a + b;
+      float diff = a - b;
+      float mult = a * b;
+      float div = a / b;
+
+
+      cout << "\nSum: " << sum; 
+      cout << "\nDifference: " << diff;  
+      cout << "\nProduct: " << mult;  
+      cout << "\nDivision: " << div;  
+    return 0;
+    
+}
